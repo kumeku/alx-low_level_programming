bb4b38f437e8211768948f1ecce318976d256771
kumeku <kumekujohn1@gmail.com>
2022-04-12 08:07:21 -0700

    wel

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,8 +13,8 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 		{
-		putchar(p[i]);
+		_putchar(p[i]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-int putchar(char c);
+int _putchar(char c);
 
 
 #endif
