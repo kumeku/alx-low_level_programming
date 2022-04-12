959a545c1f1a4a4b1a2006574e791c182b917af0
kumeku <kumekujohn1@gmail.com>
2022-04-12 13:59:09 -0700

    wd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,13 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void pr_digit(int charcode, int fmt);
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
 
 #endif
