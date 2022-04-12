72a49dd9a3681a26d5ed353e8ccc49c7475064c1
kumeku <kumekujohn1@gmail.com>
2022-04-12 14:57:50 -0700

    hmm

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,15 +14,15 @@ void print_to_98(int n)
 	{
 	for (; n > 98; n--)
 		{
-		printf("%d, ", n);
+		_putchar("%d, ", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-		printf("%d, ", n);
+		_putchar("%d, ", n);
 		}
 	}
-	printf("%d\n", n);
+	_putchar("%d\n", n);
 }
