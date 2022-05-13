b66b60684f568d743908d58c28efb33834f74072
kumeku <101403261+kumeku@users.noreply.github.com>
2022-05-13 17:34:07 +0000

    Add files via upload

---
+++ 0x0F-function_pointer/0-print_name.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-main.c
+ * Author: John Kumeku
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: callback function that prints name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	(*f)(name);
+}
